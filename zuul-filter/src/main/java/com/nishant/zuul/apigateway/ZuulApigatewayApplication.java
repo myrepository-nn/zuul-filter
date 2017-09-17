87cf0ecd766776167e4ec23306b9d84112d24f2d
NNTutorial <nishantnig0306@gmail.com>
2017-09-17 13:07:27 +0530

    zuul filter

--- zuul-filter/src/main/java/com/nishant/zuul/apigateway/ZuulApigatewayApplication.java
+++ zuul-filter/src/main/java/com/nishant/zuul/apigateway/ZuulApigatewayApplication.java
@@ -3,12 +3,10 @@ package com.nishant.zuul.apigateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableFeignClients
 @EnableZuulProxy
 public class ZuulApigatewayApplication {
 
